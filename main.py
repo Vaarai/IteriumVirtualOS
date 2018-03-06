73946165a334b29cff067e978c2268735852f13d
Josh Wolf <joshwolfcarius@gmail.com>
2018-03-06 10:04:24 +0100

    class update

--- main.py
+++ main.py
@@ -1,25 +1,84 @@
 import paho.mqtt.client as mqtt
+import sys, getopt
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+class VirtualOS:
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+    mainTopic = "home"
+    globalTopic = "home"
+    initPayload = "$INIT$"
+    brokerip = "localhost"
+    state = 1
+    argv = ''
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    def __init__(self):
+        self.data = []
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+    def strState(self):
+        if(state):
+            return "isON"
+        else:
+            return "isOFF"
 
-client.connect("iot.eclipse.org", 1883, 60)
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+        client.subscribe(self.mainTopic)
+        client.subscribe(self.globalTopic)
+        client.publish(self.mainTopic, self.initPayload + str(self.state))
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+
+    def on_message(self, client, userdata, msg):
+        strPayload = str(msg.payload)[2:-1]
+        print("==== on_massage ====")
+        print("Incomming message : " + strPayload)
+        print("Precedent state   : " + str(self.state))
+        shouldSendState = 0
+        if(strPayload == "ON"):
+            self.state = 1
+            shouldSendState = 1
+        elif(strPayload == "OFF"):
+            self.state = 0
+            shouldSendState = 1
+        elif(strPayload == "SWITCH"):
+            self.state = not(self.state)
+            shouldSendState = 1
+        
+        if(shouldSendState):
+            client.publish(self.mainTopic, strState())
+        print("New state         : " + strPayload)
+
+    def init(self, arguments):
+        self.argv = arguments
+
+        try:
+            opts, args = getopt.getopt(self.argv,"ht:ip:",["topic=","brokerip="])
+        except getopt.GetoptError:
+            print(str(err))  # will print something like "option -a not recognized"
+            usage()
+            sys.exit(2)
+
+        for opt, arg in opts:
+            if opt == '-h':
+                print("""
+                => IteriumVirtualOS
+                    -t  --topic    : topic string
+                    -ip --brokerip : Mqtt broker ip
+                """)
+                sys.exit()
+            elif opt in ("-t", "--topic"):
+                self.mainTopic = arg
+                mainParsed = self.mainTopic.split('/')
+                self.globalTopic = mainParsed[0] + '/' + mainParsed[1]
+            elif opt in ("-ip", "--brokerip"):
+                self.brokerip = arg
+
+        client = mqtt.Client()
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+
+        client.connect(self.brokerip, 1883)
+
+        client.loop_forever()
+
+
+myOS = VirtualOS()
+myOS.init(sys.argv[1:])
